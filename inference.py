21124de3ea8232d13944cad5e9775088a56a6988
EC2 Default User <ec2-user@ip-172-16-26-158.eu-west-1.compute.internal>
2021-07-29 12:51:16 +0000

    added comments

--- inference.py
+++ inference.py
@@ -14,14 +14,17 @@ def inference_handler(event, context=None):
     model_filename = 'model.mdl'
     preprocessor_key = 'preprocess/data/preprocessing_pipeline.mdl'
     preprocessor_filename = 'preprocessing_pipeline.mdl'
-
+    
+    # Get model file from s3
     bucket = boto3.resource('s3').Bucket(bucket_name)
     bucket.download_file(Key=model_key, Filename=os.path.join(artefacts_directory, model_filename))
     model = joblib.load(os.path.join(artefacts_directory, model_filename))
     
+    # Get preprocessing pipeline from s3 
     bucket.download_file(Key=preprocessor_key, Filename=os.path.join(artefacts_directory, preprocessor_filename))    
     preprocessor = joblib.load(os.path.join(artefacts_directory, preprocessor_filename))
     
+    # Extract data from payload
     payload = event['body']
     print(payload)
     
@@ -37,12 +40,14 @@ def inference_handler(event, context=None):
     for i, p in enumerate(predictors):
         results[p] = data[i]
     
+    # Run inference
     X = np.reshape(data, (1, len(predictors)))
     Xt = preprocessor.transform(X)
     prediction = model.predict(Xt)[0]
     
     results['prediction'] = prediction
     
+    # Put results in dynamodb
     results_table = 'hastie'
     table = boto3.resource('dynamodb').Table(results_table)
     status = table.put_item(Item=dict_to_dynamodb(results))
